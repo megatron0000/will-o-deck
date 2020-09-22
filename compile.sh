d105dec91864555c0bd4615f60af0640410d7818
Vitor Arruda <vitor_pimenta97@hotmail.com>
2020-09-22 23:43:01 +0000

    Ajuste de parametros para rodada teste

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ exact.cpp -O3 -o exact
--- exact.cpp
+++ exact.cpp
@@ -2,43 +2,76 @@
 #include <iostream>
 
 double card_attributes[][7] = {
-    {1, 1, 1, 1, 1, 1, 1}, {1, 1, 0, 2, 2, 1, 1}, {2, 2, 1, 1, 2, 1, 2},
-    {0, 1, 2, 2, 0, 2, 2}, {3, 2, 0, 0, 0, 2, 0}, {1, 1, 1, 1, 1, 0, 0},
-    {1, 0, 2, 0, 1, 1, 1}, {1, 1, 0, 2, 1, 2, 2}, {0, 1, 0, 2, 3, 0, 0},
-    {0, 1, 0, 2, 3, 0, 0}, {1, 1, 0, 3, 2, 1, 1}, {2, 3, 0, 3, 2, 1, 0},
-    {1, 3, 0, 3, 2, 1, 1}, {1, 2, 0, 3, 1, 1, 1}, {1, 2, 1, 3, 2, 2, 0},
-    {3, 1, 2, 2, 2, 1, 1}, {3, 0, 2, 1, 1, 2, 0}, {3, 1, 1, 2, 1, 0, 1},
-    {3, 2, 3, 3, 2, 3, 2}, {3, 1, 2, 3, 3, 3, 2}, {0, 3, 1, 1, 0, 0, 3},
-    {2, 1, 1, 1, 1, 2, 0}, {2, 1, 0, 1, 0, 0, 0}, {2, 2, 1, 1, 1, 1, 1},
-    {3, 2, 1, 2, 1, 3, 2}, {3, 1, 2, 2, 3, 3, 1}, {1, 2, 2, 2, 2, 1, 2},
-    {2, 1, 3, 3, 3, 3, 1}, {3, 2, 2, 1, 2, 3, 0}, {3, 2, 3, 3, 3, 3, 3},
-    {2, 0, 2, 3, 1, 3, 3}, {1, 3, 1, 2, 3, 3, 0}, {0, 3, 1, 2, 3, 2, 2},
-    {0, 3, 0, 1, 2, 0, 3}, {3, 3, 0, 2, 1, 3, 0}, {1, 0, 2, 1, 1, 3, 3},
-    {1, 1, 3, 1, 2, 0, 0}, {0, 1, 3, 0, 1, 0, 3}, {3, 3, 2, 2, 3, 3, 2},
-    {3, 2, 3, 2, 1, 1, 0}, {1, 1, 2, 1, 1, 1, 1}, {1, 1, 0, 3, 2, 2, 1},
-    {1, 1, 1, 1, 1, 1, 1}, {1, 0, 3, 2, 1, 3, 1}, {1, 1, 0, 2, 1, 3, 0},
-    {0, 1, 1, 2, 2, 0, 3}, {0, 1, 1, 3, 3, 2, 0}, {0, 2, 1, 3, 1, 1, 1},
-    {2, 2, 2, 2, 2, 2, 2}, {0, 0, 1, 3, 2, 0, 0}, {1, 1, 1, 1, 1, 1, 0},
-    {1, 1, 1, 1, 1, 1, 1}, {1, 2, 3, 2, 3, 3, 1}, {0, 3, 3, 1, 1, 3, 3},
-    {1, 2, 3, 1, 3, 3, 2}, {0, 1, 2, 2, 3, 3, 1}, {0, 2, 0, 1, 1, 2, 2},
-    {0, 1, 2, 2, 3, 0, 3}, {3, 1, 3, 2, 3, 3, 1}, {1, 1, 1, 1, 1, 1, 1},
-    {0, 1, 2, 1, 1, 1, 1}, {1, 1, 3, 2, 1, 1, 2}, {1, 3, 1, 3, 0, 0, 3},
-    {0, 3, 0, 1, 1, 0, 3}, {0, 0, 0, 1, 0, 1, 3}, {1, 0, 1, 3, 0, 0, 3},
-    {0, 0, 0, 2, 0, 2, 3}, {0, 0, 0, 2, 1, 0, 3}, {2, 2, 3, 2, 3, 2, 0},
-    {3, 3, 3, 3, 3, 3, 3}, {3, 1, 3, 2, 3, 1, 1}, {1, 1, 1, 1, 1, 1, 1}};
+    {1, 1, 1, 1, 1, 1, 1},
+    {2, 2, 2, 2, 2, 2, 2},
+    {0, 0, 1, 0, 1, 0, 1},
+    {0, 1, 2, 0, 2, 2, 2},
+    {2, 2, 0, 0, 0, 2, 0},
+    {2, 2, 1, 1, 1, 0, 0},
+    {3, 0, 2, 1, 0, 3, 1},
+    {3, 1, 0, 1, 3, 3, 2},
+    {2, 1, 1, 1, 1, 2, 0},
+    {2, 1, 0, 1, 0, 0, 0},
+    {2, 2, 1, 1, 1, 1, 1},
+    {3, 2, 1, 2, 1, 3, 2},
+    {3, 1, 2, 2, 3, 3, 1},
+    {1, 2, 2, 2, 2, 1, 2},
+    {3, 2, 3, 2, 1, 1, 0},
+    {1, 1, 2, 1, 1, 1, 1},
+    {1, 0, 2, 0, 1, 1, 2},
+    {1, 1, 0, 3, 2, 2, 1},
+    {3, 3, 1, 2, 1, 0, 0},
+    {0, 2, 0, 1, 1, 2, 2},
+    {0, 1, 2, 2, 3, 0, 3},
+    {3, 1, 3, 2, 3, 3, 1},
+    {1, 1, 1, 1, 1, 1, 1},
+    {0, 1, 2, 1, 1, 1, 1},
+    {1, 1, 3, 2, 1, 1, 2},
+    {1, 3, 1, 3, 0, 0, 3},
+    {0, 3, 0, 1, 1, 0, 3},
+    {0, 0, 0, 1, 0, 1, 3},
+    {1, 0, 1, 3, 0, 0, 3},
+    {0, 0, 0, 2, 0, 2, 3},
+    {0, 0, 0, 2, 1, 0, 3}};
 
 double card_costs[] = {
-    350, 400, 550, 450, 350, 250, 300, 450, 300, 300,  450, 550, 550, 450, 550,
-    600, 450, 450, 900, 850, 400, 400, 200, 450, 700,  750, 600, 800, 650, 1000,
-    700, 650, 650, 450, 600, 550, 400, 400, 900, 600,  400, 500, 350, 550, 400,
-    450, 500, 450, 700, 300, 300, 350, 750, 700, 750,  600, 400, 550, 800, 350,
-    350, 550, 550, 400, 250, 400, 350, 300, 700, 1050, 700, 350};
+    350,
+    700,
+    150,
+    450,
+    300,
+    350,
+    500,
+    650,
+    400,
+    200,
+    450,
+    700,
+    750,
+    600,
+    600,
+    400,
+    350,
+    500,
+    500,
+    400,
+    550,
+    800,
+    350,
+    350,
+    550,
+    550,
+    400,
+    250,
+    400,
+    350,
+    300};
 
 const int quant_cards = sizeof(card_costs) >> 3;
 
-double group_last_cards[] = {19, 38, 55, 71};
+double group_last_cards[] = {7, 13, 18, 30};
 
-int group_lengths[] = {20, 19, 17, 16};
+int group_lengths[] = {8, 6, 5, 12};
 
 double expected[6] = {19, 7, 11, 15, 17, 16};
 
@@ -50,7 +83,8 @@ double best_profit = -1;
 
 unsigned long int count = 0;
 
-void process(int group_count, int* group_lengths, int total_length, int* data) {
+void process(int group_count, int *group_lengths, int total_length, int *data)
+{
   // int start = 0;
   // for (int group = 0; group < group_count; group++) {
   //   int group_length = group_lengths[group];
@@ -69,10 +103,13 @@ void process(int group_count, int* group_lengths, int total_length, int* data) {
   double total_attr[7] = {0, 0, 0, 0, 0, 0, 0};
   int repeated = data[3] + data[7] + data[33] + data[37] + data[38] + data[53] +
                  data[54] + data[69];
-  if (repeated > 4) return;
+  if (repeated > 4)
+    return;
   // bool has_group[] = {false, false, false, false};
-  for (int i = 0; i < total_length; i++) {
-    if (data[i] == 0) continue;  // card is not present
+  for (int i = 0; i < total_length; i++)
+  {
+    if (data[i] == 0)
+      continue; // card is not present
     // card_total++;
     // assert(card_total <= 8);
     // has_group[i <= group_last_cards[0]
@@ -81,7 +118,8 @@ void process(int group_count, int* group_lengths, int total_length, int* data) {
     //                     ? 1
     //                     : i <= group_last_cards[2] ? 2 : 3] = true;
     spent_total += card_costs[i];
-    for (int j = 0; j < 7; j++) {
+    for (int j = 0; j < 7; j++)
+    {
       total_attr[j] += card_attributes[i][j];
     }
   }
@@ -91,70 +129,86 @@ void process(int group_count, int* group_lengths, int total_length, int* data) {
 
   double inner_product = 0;
   for (int j = 0; j < 6;
-       j++) {  // until 6 because "attribute cost" does not count
+       j++)
+  { // until 6 because "attribute cost" does not count
     inner_product +=
         (total_attr[j] < expected[j] ? total_attr[j] : expected[j]);
   }
   inner_product /= expected_total;
 
-  if (total_attr[6] > 20) return;
+  if (total_attr[6] > 20)
+    return;
 
   double pitch_score = 0.8;
   double cost_ease = (1 - pitch_score * total_attr[6] * 0.02);
-  if (cost_ease <= 0) return;
+  if (cost_ease <= 0)
+    return;
 
   double remaining = 10133 - spent_total;
-  if (remaining <= 0) return;
+  if (remaining <= 0)
+    return;
 
   double profit =
       (1 / (100 * cost_ease)) * remaining * 250 * inner_product * pitch_score;
 
-  if (profit > best_profit) {
+  if (profit > best_profit)
+  {
     best_profit = profit;
-    for (int i = 0; i < quant_cards; i++) {
+    for (int i = 0; i < quant_cards; i++)
+    {
       best[i] = data[i];
     }
   }
 
   count++;
-  if (count % 1000000 == 0) {
+  if (count % 1000000 == 0)
+  {
     std::cout << "count: " << count << ". profit: " << best_profit << std::endl;
   }
 }
 
-void cycle_multi_one(int group, int group_count, int* group_lengths,
-                     int total_length, int* out, int start, int end, int n,
-                     bool group_has_one) {
+void cycle_multi_one(int group, int group_count, int *group_lengths,
+                     int total_length, int *out, int start, int end, int n,
+                     bool group_has_one)
+{
   int length = end - start + 1;
   int my_group_start = 0;
-  for (int i = 0; i < group; i++) {
+  for (int i = 0; i < group; i++)
+  {
     my_group_start += group_lengths[i];
   }
-  if (n == 0) {
-    for (int i = start; i <= end; i++) {
+  if (n == 0)
+  {
+    for (int i = start; i <= end; i++)
+    {
       out[i] = 0;
     }
     process(group_count, group_lengths, total_length, out);
     return;
   }
-  if (n > length) {  // too many to choose. Impossible
+  if (n > length)
+  { // too many to choose. Impossible
     return;
   }
   if (n == group_count - group - 1 &&
-      group_has_one) {  // n is small. Can fit at most one per group, and my
-                        // group already has one. Forward to the next group
+      group_has_one)
+  { // n is small. Can fit at most one per group, and my
+    // group already has one. Forward to the next group
     int next_group_start = 0;
     next_group_start = my_group_start + group_lengths[group];
-    for (int i = start; i < next_group_start; i++) {
+    for (int i = start; i < next_group_start; i++)
+    {
       out[i] = 0;
     }
     cycle_multi_one(group + 1, group_count, group_lengths, total_length, out,
                     next_group_start, end, n, false);
     return;
   }
-  if (length == n) {  // I can only choose all
+  if (length == n)
+  { // I can only choose all
     int next_group_start = my_group_start + group_lengths[group];
-    for (int i = start; i < next_group_start; i++) {
+    for (int i = start; i < next_group_start; i++)
+    {
       out[i] = 1;
     }
     cycle_multi_one(group + 1, group_count, group_lengths, total_length, out,
@@ -163,11 +217,13 @@ void cycle_multi_one(int group, int group_count, int* group_lengths,
     return;
   }
   if (start == my_group_start + group_lengths[group] - 1 &&
-      group != group_count - 1) {  // I am in the last position of the
-                                   // group (except in the last group)
-    if (!group_has_one) {  // No one chose an element of the group and I have to
-                           // do it because I am in the last position of the
-                           // group
+      group != group_count - 1)
+  { // I am in the last position of the
+    // group (except in the last group)
+    if (!group_has_one)
+    { // No one chose an element of the group and I have to
+      // do it because I am in the last position of the
+      // group
       out[start] = 1;
       cycle_multi_one(group + 1, group_count, group_lengths, total_length, out,
                       start + 1, end, n - 1, false);
@@ -184,8 +240,9 @@ void cycle_multi_one(int group, int group_count, int* group_lengths,
   if (group == group_count - 1 && !group_has_one &&
       start ==
           my_group_start + group_lengths[group] -
-              1) {  // No one chose an element of the group and I have to
-                    // do it because I am in the last position of the last group
+              1)
+  { // No one chose an element of the group and I have to
+    // do it because I am in the last position of the last group
     out[start] = 1;
     process(group_count, group_lengths, total_length, out);
     return;
@@ -198,24 +255,29 @@ void cycle_multi_one(int group, int group_count, int* group_lengths,
                   start + 1, end, n - 1, true);
 }
 
-void cycle_multi(int group_count, int* group_lengths, int total_length,
-                 int* out, int n) {
+void cycle_multi(int group_count, int *group_lengths, int total_length,
+                 int *out, int n)
+{
   cycle_multi_one(0, group_count, group_lengths, total_length, out, 0,
                   total_length - 1, n, false);
 }
 
-int main() {
-  int* arr = new int[quant_cards];
+int main()
+{
+  int *arr = new int[quant_cards];
   cycle_multi(4, group_lengths, quant_cards, arr, 8);
 
   int start = 0;
-  for (int group = 0; group < 4; group++) {
+  for (int group = 0; group < 4; group++)
+  {
     int group_length = group_lengths[group];
-    for (int i = start; i < start + group_length; i++) {
+    for (int i = start; i < start + group_length; i++)
+    {
       std::cout << best[i] << " ";
     }
     start += group_length;
-    if (group != 4 - 1) {
+    if (group != 4 - 1)
+    {
       std::cout << ". ";
     }
   }
